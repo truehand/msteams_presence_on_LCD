948e8af6518435283cb8d573d720b485d5de749a
Mutlu Dogruel <truehand@gmail.com>
2021-12-27 23:35:52 +0000

    removed cpu temp fnc

--- text_on_LCD.py
+++ text_on_LCD.py
@@ -9,26 +9,20 @@ from datetime import datetime
 PCF8574_address = 0x27  # I2C address of the PCF8574 chip.
 PCF8574A_address = 0x3F  # I2C address of the PCF8574A chip.
 
-def get_cpu_temp():     # get CPU temperature and store it into file "/sys/class/thermal/thermal_zone0/temp"
-    tmp = open('/sys/class/thermal/thermal_zone0/temp')
-    cpu = tmp.read()
-    tmp.close()
-    return '{:.2f}'.format( float(cpu)/1000 ) + ' C'
-
 class LCD():
     def __init__(self):
         # Create PCF8574 GPIO adapter.
         try:
-            mcp = PCF8574_GPIO(PCF8574_address)
+            self.mcp = PCF8574_GPIO(PCF8574_address)
         except:
             try:
-                mcp = PCF8574_GPIO(PCF8574A_address)
+                self.mcp = PCF8574_GPIO(PCF8574A_address)
             except:
                 print ('I2C Address Error !')
                 exit(1)
-        mcp.output(3,1)     # turn on LCD backlight
+        self.mcp.output(3,1)     # turn on LCD backlight
 
-        self.lcd = Adafruit_CharLCD(pin_rs=0, pin_e=2, pins_db=[4,5,6,7], GPIO=mcp)
+        self.lcd = Adafruit_CharLCD(pin_rs=0, pin_e=2, pins_db=[4,5,6,7], GPIO=self.mcp)
         self.lcd.begin(16,2)     # set number of LCD lines and columns
         self.isLoop = True
 
@@ -64,3 +58,5 @@ class LCD():
 
     def destroy(self):
         self.lcd.clear()
+        self.mcp.output(0,0)     # turn off LCD backlight
+
