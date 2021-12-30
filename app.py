617c654a223a58fa65ca9541e767434c2fd734c2
Mutlu Dogruel <truehand@gmail.com>
2021-12-30 21:13:44 +0000

    return value changed to the most recent status

--- app.py
+++ app.py
@@ -71,8 +71,7 @@ def status_message(status):
         sleep(2)
         myLcd.setLoop(True)
         myLcd.msg(status[4:])
-    #return render_template('index.html', value = status)
-    return "200 OK"
+    return render_template('index.html', value = recentStatus), 200
 
 @app.route('/')
 def get_status():
