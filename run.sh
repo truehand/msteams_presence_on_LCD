33b7c92b680ee6c3feefc97f8c03806023b2f4b2
Mutlu Dogruel <truehand@gmail.com>
2021-12-31 13:22:00 +0000

    adding new messages to previous logs

--- run.sh
+++ run.sh
@@ -8,6 +8,6 @@ echo running the Flask application on port 5000...
 export FLASK_ENV=development
 export FLASK_APP=presence_server.py
 mkdir -p logs
-nohup flask run -h 0.0.0.0 -p 5000 2>logs/err.txt 1>logs/log.txt&
+nohup flask run -h 0.0.0.0 -p 5000 2>>logs/err.txt 1>>logs/log.txt&
 echo logs are in logs/log.txt and logs/err.txt
 echo done.
