6c271135dc6e1a12d01268244e52d183acaf156c
Mutlu Dogruel <truehand@gmail.com>
2022-01-05 13:12:28 +0000

    pkill used to kill flask and rq if already running

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
-sudo service redis-server start
-echo killing a previous potential Flask session...
-ps -ef|grep "flask run"|tr -s " "|cut -d " " -f 2| head -1| xargs kill
+sudo service redis-server restart
+echo killing previous potential Flask sessions...
+pkill -9 -f "flask run"
 sleep 1
 echo pulling the latest code...
 git pull
@@ -11,7 +11,7 @@ export FLASK_APP=presence_server.py
 mkdir -p logs
 nohup flask run -h 0.0.0.0 -p 5000 2>>logs/err.txt 1>>logs/log.txt&
 echo killing previous rq workers
-killall rq
+pkill -9 -f "rq worker"
 echo starting worker
 rq worker >>logs/worker.txt 2>>logs/err.txt&
 echo logs are in logs/log.txt, logs/worker.txt and logs/err.txt
